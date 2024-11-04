22ffc8160b333bcb540c83b4a0e63b53b4d8f3c6
antonioluque27 <antonioluque.glasgow@gmail.com>
2024-11-04 17:27:17 +0100

    Update Projecte_Final_v3.py

--- Projecte_Final_v3.py
+++ Projecte_Final_v3.py
@@ -329,8 +329,8 @@ with col_Data_2:
 	     #if add_radio == No Passed:
              text = "The analysis of the requirement showed a device's failure to achieve the task."
              text_input = st.text_input("Enter Conclusion of the Test 👇")
-            	 if text_input:
-             		st.write("You have entered: "+ text + text_input)
+             if text_input:
+             	st.write("You have entered: "+ text + text_input)
 
     elif is_wind == "FRT for HVRT":
                 ####--Create a Figure to be added 
