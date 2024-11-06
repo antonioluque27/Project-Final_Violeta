02123f59484a2cfe6edffc9f05269269f25b712f
antonioluque27 <antonioluque.glasgow@gmail.com>
2024-11-06 12:59:12 +0100

    Update Projecte_Final_v3.py

--- Projecte_Final_v3.py
+++ Projecte_Final_v3.py
@@ -153,11 +153,14 @@ with col_Req_1:
 # # TEXT TO BE INTRODUCE BELOW each Requirement
 # =============================================================================
         if is_wind == "Active Power":
-            text_P = st.write('''The ability of the wind turbine to operate in active power control mode shall be characterized
-                       for various reference values given by the control interface. This test aims to determine
-                       the response of the WT to reference commands regarding the static error, the rise time and
-                       the settling time of active power, for both steady-state conditions and under dynamic
-                        response conditions.''')
+            text_P = st.write('''Procedure:
+• Measurements shall be sampled during continuous operation only.
+• The active and reactive power shall be measured at the WT's terminals.
+• The active power during the entire control test shall be in the range where the WT can
+reach its maximum reactive power.
+• The sampled data for reactive power shall be one fundamental period's average data.
+• The measured reactive power shall be shown in a graph as one fundamental period data
+together with the reference value of reactive power''')
                                     
         elif is_wind == "Reactive Power":
             text_Q = st.write(''' The aim of this test is to determine the response of the WT to reference commands regarding
