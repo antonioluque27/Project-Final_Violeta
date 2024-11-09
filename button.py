0553109c6b9755b6401c534fb206b371657b5fc5
antonioluque27 <antonioluque.glasgow@gmail.com>
2024-11-09 15:53:48 +0100

    Update button.py

--- button.py
+++ button.py
@@ -15,7 +15,6 @@ import subprocess
 import os
 
 
-
 with st.form("my_form"):
     st.write("Inside the form")
     slider_val = st.slider("Form slider")
@@ -25,6 +24,16 @@ with st.form("my_form"):
 #if submitted:
 #    st.write("slider", slider_val, "checkbox", checkbox_val)
 #st.write("Outside the form")
+
+import streamlit as st
+
+animal_shelter = ['cat', 'dog', 'rabbit', 'bird']
+
+animal = st.text_input('Type an animal')
+
+if st.button('Check availability'):
+    have_it = animal.lower() in animal_shelter
+    'We have that animal!' if have_it else 'We don\'t have that animal.'
 
 
 
