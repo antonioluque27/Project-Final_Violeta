215bdd8bfe3e5aa9bda2eae607e86a1d64bd310d
antonioluque27 <antonioluque.glasgow@gmail.com>
2024-11-09 15:42:30 +0100

    Update button.py

--- button.py
+++ button.py
@@ -44,7 +44,17 @@ if sentence:
         st.write(text2)
 
 
-#bt1=st.button('Submit')
+with st.form("my_form"):
+    st.write("Inside the form")
+    slider_val = st.slider("Form slider")
+    checkbox_val = st.checkbox("Form checkbox")
+# Every form must have a submit button.
+    submitted = st.form_submit_button("Submit")
+if submitted:
+    st.write("slider", slider_val, "checkbox", checkbox_val)
+st.write("Outside the form")
+
+
 
 #if st.session_state.get('button') != True:
 #            st.session_state['button'] = bt1
